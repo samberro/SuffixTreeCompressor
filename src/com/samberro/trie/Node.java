bbd720c2d0fe4b5e6d6f6f7907fbfb3ce06fbe7e
Berro, Sam <sam.berro@sap.com>
2020-12-15 10:13:35 -0800

    Code cleanup

--- src/com/samberro/trie/Node.java
+++ src/com/samberro/trie/Node.java
@@ -6,7 +6,7 @@ import static com.samberro.trie.SuffixTrie.MAX_DISTANCE;
 
 public class Node {
     public static int COUNT = 0;
-    private static Node topHeap, bottomHeap, recycleTop;
+    private static Node heapTop, heapBottom, recycleTop;
 
     //    private Node[] nodes = new Node[256];
     private HashMap<Byte, Node> nodes = new HashMap<>();
@@ -18,8 +18,6 @@ public class Node {
     private Node previousInHeap;
     private Node nextInHeap;
     private Node parentNode;
-
-    boolean recycled = false;
 
     private Node(byte val, int depth, Node parentNode) {
         this.val = val;
@@ -39,7 +37,6 @@ public class Node {
         n.depth = depth;
         n.parentNode = parentNode;
 
-        n.recycled = false;
         n.lastSeenIndex = -1;
         n.nextSuffix = null;
         n.previousInHeap = null;
@@ -48,7 +45,7 @@ public class Node {
     }
 
     private static void moveRecycledTop() {
-        recycleTop = recycleTop.nextInHeap == topHeap ? null : recycleTop.nextInHeap;
+        recycleTop = recycleTop.nextInHeap == heapTop ? null : recycleTop.nextInHeap;
     }
 
     private static boolean hasRecycled() {
@@ -56,76 +53,65 @@ public class Node {
     }
 
     public int getLastSeenIndex() {
-        assertNotRecycled();
         return lastSeenIndex;
     }
 
     public void setLastSeenIndex(int lastSeenIndex) {
-        assertNotRecycled();
         this.lastSeenIndex = lastSeenIndex;
         moveToBottomOfHeap();
     }
 
     private void moveToBottomOfHeap() {
-        assertNotRecycled();
+        if (heapBottom == this) return;
 
-        if (bottomHeap == this) return;
-        if (bottomHeap == null) {
-            bottomHeap = this;
-            topHeap = this;
+        // First node created. Initialize heap
+        if (heapTop == null) {
+            heapTop = this;
+            heapBottom = this;
             return;
         }
-
+        // Connect previous and next
         if (previousInHeap != null) previousInHeap.nextInHeap = nextInHeap;
         if (nextInHeap != null) nextInHeap.previousInHeap = previousInHeap;
-        if (topHeap == this) topHeap = nextInHeap;
-        if (recycleTop == this) moveRecycledTop();
+        // Move heapTop and recycle pointers if we need to
+        if (heapTop == this) heapTop = nextInHeap;
 
-        bottomHeap.nextInHeap = this;
-        previousInHeap = bottomHeap;
+        previousInHeap = heapBottom;
+        heapBottom.nextInHeap = this;
+        heapBottom = this;
         nextInHeap = null;
-        bottomHeap = this;
 
-        if (lastSeenIndex - topHeap.lastSeenIndex > (MAX_DISTANCE)) dropStale(lastSeenIndex - MAX_DISTANCE);
+        if (lastSeenIndex - heapTop.lastSeenIndex > (MAX_DISTANCE)) dropStale(lastSeenIndex - MAX_DISTANCE);
     }
 
     private void dropStale(int dropIndex) {
-        assertNotRecycled();
-        while (topHeap.lastSeenIndex < dropIndex) topHeap.parentNode.removeNode(topHeap);
+        while (heapTop.lastSeenIndex < dropIndex) {
+            heapTop.parentNode.removeNode(heapTop);
+            moveHeapTop();
+        }
     }
 
     public Node nodeAt(byte b) {
-        assertNotRecycled();
 //        return nodes[b&0xFF];
         return nodes.get(b);
     }
 
     public int getDepth() {
-        assertNotRecycled();
         return depth;
     }
 
     public Node getNextSuffixLink() {
-        assertNotRecycled();
         return nextSuffix;
     }
 
     public void setNextSuffixLink(Node nextSuffix) {
-        assertNotRecycled();
         this.nextSuffix = nextSuffix;
     }
 
     public void addNode(byte b, Node node) {
-        assertNotRecycled();
         if (nodeAt(b) != null) throw new RuntimeException("Replacing is not allowed");
         nodes.put(b, node);
 //        nodes[b&0xFF] = node;
-    }
-
-    public Collection<Node> getNodes() {
-        assertNotRecycled();
-        return nodes.values();
-//        return Arrays.asList(nodes);
     }
 
     public void removeNode(Node n) {
@@ -133,33 +119,12 @@ public class Node {
         nodes.remove(n.val);
         n.parentNode = null;
 
-        removeFromHeap(n);
         n.nodes.clear();
-        n.recycle();
     }
 
-    private static void removeFromHeap(Node n) {
-        if (n == topHeap) {
-            topHeap = n.nextInHeap;
-        } else if (n == bottomHeap) {
-            bottomHeap = n.previousInHeap;
-            if (bottomHeap != null) bottomHeap.nextInHeap = null;
-            throw new RuntimeException("bottom");
-        } else {
-            throw new RuntimeException("Middle");
-//            n.previousInHeap.nextInHeap = n.nextInHeap;
-//            n.nextInHeap.previousInHeap = n.previousInHeap;
-        }
-
-        if (recycleTop == null) recycleTop = n;
-    }
-
-    private void recycle() {
-        recycled = true;
-    }
-
-    private void assertNotRecycled() {
-        if (recycled) throw new RuntimeException("Recycled");
+    private static void moveHeapTop() {
+        if (recycleTop == null) recycleTop = heapTop;
+        heapTop = heapTop.nextInHeap;
     }
 
     @Override
